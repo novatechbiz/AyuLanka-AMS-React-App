b942420dfb8181842973905e43e0177c4fcb9738
anuradha-itworx <anuradha@itworx.com.au>
2025-11-02 18:19:36 +0530

    dfdf

--- src/components/tokenGenerate/TokenGenerate.jsx
+++ src/components/tokenGenerate/TokenGenerate.jsx
@@ -91,7 +91,7 @@ function TokenGenerate() {
 
     // Read values from env
     const rows = Number(process.env.REACT_APP_TOTAL_ROWS) || 15;
-    const cols = Number(process.env.REACT_APP_TOKENS_PER_ROW) || 6;
+    const cols = Number(process.env.REACT_APP_TOKENS_PER_ROW) || 8;
     const startHour = Number(process.env.REACT_APP_APPOINTMENT_START_HOUR) || 7; // 7 AM
     const totalTokens = rows * cols;
 
